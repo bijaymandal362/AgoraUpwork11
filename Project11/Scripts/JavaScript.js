a93438c22b017e291bc1109184b63ca86185cc69
bijaymandal362 <56814704+bijaymandal362@users.noreply.github.com>
2020-11-11 20:10:50 +0545

    AgoraUpwork V1
    AgoraUpwork V1

---
+++ Project11/Scripts/JavaScript.js
@@ -0,0 +1,31 @@
+﻿/**
+ * @name handleFail
+ * @param err - error thrown by any function
+ * @description Helper function to handle errors
+ */
+let handleFail = function (err) {
+    console.log("Error : ", err);
+};
+
+// Queries the container in which the remote feeds belong
+let remoteContainer = document.getElementById("remote-container");
+/**
+ * @name addVideoStream
+ * @param streamId
+ * @description Helper function to add the video stream to "remote-container"
+ */
+function addVideoStream(streamId) {
+    let streamDiv = document.createElement("div"); // Create a new div for every stream
+    streamDiv.id = streamId;                       // Assigning id to div
+    streamDiv.style.transform = "rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
+    remoteContainer.appendChild(streamDiv);      // Add new div to container
+}
+/**
+ * @name removeVideoStream
+ * @param evt - Remove event
+ * @description Helper function to remove the video stream from "remote-container"
+ */
+function removeVideoStream(streamId) {
+    let remDiv = document.getElementById(streamId);
+    if (remDiv) remDiv.parentNode.removeChild(remDiv);
+}
